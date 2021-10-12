eb5613dd37fa0e9d1ae259bede29a7c20b007f6b
I-the-Creator <andrey.burluckiy@gmail.com>
2021-10-12 15:32:50 +0200

    posts numbering changed (from number to post.id)

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import MyButton from './UI/button/MyButton';
 
 
-    // получаем props on родителя - PostList
+    // получаем props oт родителя - PostList
 const PostItem = (props) => {
     // console.log(props);   // debug -  получаем список объектов (постов)
     return (
         <div className="post">
             <div className="post__content">
                 {/* number и title получаем из PostList */}
-                <strong>{props.number}. {props.post.title}</strong>
+                <strong>{props.post.id}. {props.post.title}</strong>
                 <div>
                     {props.post.body}
                 </div>

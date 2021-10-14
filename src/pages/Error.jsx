4e8d4e1932bab38a0be35216ed77f9a031a056d6
I-the-Creator <andrey.burluckiy@gmail.com>
2021-10-14 14:35:19 +0200

    '/error' page added

---
+++ src/pages/Error.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <h1 style={{color: 'red'}}>
+                You're tying to open non-existing page!
+            </h1>
+        </div>
+    );
+};
+
+export default Error;

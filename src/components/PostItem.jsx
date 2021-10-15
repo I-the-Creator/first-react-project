fb7042729446d972a110bf73b10873af89eef617
I-the-Creator <andrey.burluckiy@gmail.com>
2021-10-15 14:50:22 +0200

    new button 'Open' added to page with posts to open selected post in new window

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import MyButton from './UI/button/MyButton';
 
 
     // получаем props oт родителя - PostList
 const PostItem = (props) => {
+
+    // console.log(props);  // список объектов Posts
+
+    const router = useHistory()
+    // console.log(router)   // список объектов маршрутизации для каждого объекта post со свойствами push, go, location... 
+
     // console.log(props);   // debug -  получаем список объектов (постов)
     return (
         <div className="post">
@@ -15,6 +22,12 @@ const PostItem = (props) => {
                 </div>
             </div>
             <div className="post__btns">
+                {/* router.push - переход на конкреиную страницу без использования ссылок */}
+                {/* например пр нажатии на кнопку или при любом другом событии */}
+                {/* формируется динамический путь к конкретному посту */}
+                <MyButton onClick={() => router.push(`/posts/${props.post.id}`)}>
+                    Open
+                </MyButton>
                 {/* удаляем элемент массива с заданным id - проверк id происходит в функции 'removePost' в App.js*/}
                 <MyButton onClick={() => props.remove(props.post)}>Delete</MyButton>
             </div>

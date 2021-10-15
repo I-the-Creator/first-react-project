70d2fe2b31f9cc9fc6dc41e0933bfd6b468e2d4b
I-the-Creator <andrey.burluckiy@gmail.com>
2021-10-15 18:23:33 +0200

    AppRouter optimized uzing 'routes.js'

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,31 +1,27 @@
 import React from 'react';
-import { Redirect, Switch, Route } from 'react-router';
+import { Redirect, Switch, Route, Router } from 'react-router';
 import About from '../pages/About';
 import Error from '../pages/Error';
 import PostIdPage from '../pages/PostIdPage';
 import Posts from '../pages/Posts';
+import { routes } from '../router/router';
 
 const AppRouter = () => {
     return (
         <Switch>
         {/* connect component 'route' from react-router-dom  */}
         {/* add path to target page - what will be in a URL after domain name */}
-            <Route path="/about">   
-                <About/>
-            </Route>
-            <Route exact path="/posts">   
-                <Posts/>
-            </Route>
-
-            {/* динамический маршрут к выбранному посту по кнопке 'Open' */}
-            <Route exact path="/posts/:id">   
-                <PostIdPage/>
-            </Route>
-
-            <Route path="/error">
-                <Error/>
-            </Route>
-            <Redirect to='/error'/>
+        {/* итеррируемся по массиву маршрутов  и для каждого элеиента массива отрисовываем маршрут*/}
+            {routes.map(route => 
+                // component, path и exact достаем из объекта маршрутов и передаем их как props
+                <Route
+                    component={route.component}
+                    path={route.path}
+                    exact={route.exact}
+                />
+            )}
+            {/* редирект по дефолту - если такой страницы нет или при переходе на общий url домена*/}
+            <Redirect to='/posts'/>   
         </Switch>
     );
 };
